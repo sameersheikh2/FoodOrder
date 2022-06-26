1fe5c72bb718a8064eb71dfb838814d4d896288b
sameersheikh2 <sameersheikh819@gmail.com>
2022-06-26 17:31:54 +0530

    added input as reusable ui , passed meal list to mealItem to render'

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css'
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -28,17 +30,24 @@ const DUMMY_MEALS = [
     },
 ];
 
-export const AvailableMeals = () => {
+export const AvailableMeals = (props) => {
     const mealsList =
-        DUMMY_MEALS.map(meal => 
-            <li>{meal.name}</li>
+        DUMMY_MEALS.map(meal =>
+            <MealItem key={meal.id} 
+            id={meal.id} // this is new!
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+            />
         )
 
     return (
         <section className={classes.meals}>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
